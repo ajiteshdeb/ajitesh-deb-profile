9fac52ef2ebd0db199ee620d7831f44aa7b4b958
Ajitesh Deb <ajitesh.deb@gmail.com>
2020-01-29 00:13:12 +0530

    lottie added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Lottie from 'react-lottie';
+import animationData from './assests/cat.json'
 import './App.css';
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true, 
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Lottie 
+        options={defaultOptions}
+        height={400}
+        width={400}
+      />
+      <h1>Coming Soon</h1>
       </header>
     </div>
   );
